b536b944263f21015bbbb519a0645ae8887dee14
Samuel Leonel <samueleonelfreitas@hotmail.com>
2020-06-03 16:01:13 -0300

    WEB - Adição de roteamento e adição de rota de criar pontos

---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+const CreatePoint = () => {
+  return (
+    <h1>Hello point</h1>
+  )
+}
+
+export default CreatePoint;
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import './style.css';
+import { Link } from "react-router-dom";
 import { FiLogIn } from 'react-icons/fi'
+import './style.css';
 import logo from '../../assets/logo.svg'
 
 const Home = () => {
@@ -13,12 +14,12 @@ const Home = () => {
         <main>
           <h1>Seu marketplace de coleta de resíduos.</h1>
           <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
-          <a href="/cadastro">
+          <Link to="/criar-ponto">
             <span>
               <FiLogIn />
             </span>
             <strong>Cadastre um ponto de coleta</strong>
-          </a>
+          </Link>
         </main>
       </div>
     </div>
